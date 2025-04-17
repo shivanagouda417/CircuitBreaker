ef36ecc66232778363bedd36ae972cfc455b179a
shivanagouda <shivarp02@gmail.com>
2025-04-17 07:49:49 +0530

    removed kavita

--- RebaseExample.java
+++ RebaseExample.java
@@ -2,5 +2,6 @@ public class RebaseExample {
     public static void main(String[] args) {
         System.out.println("welcome to shiva");
         System.out.println("welcome to Bhagya");
+        System.out.println("welcome to kabita");
     }
 }
