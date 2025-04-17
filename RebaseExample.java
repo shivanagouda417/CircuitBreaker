43fd452c7c5d5672af81ba0829e9995bfb157681
shivanagouda patil <shivarp02@gmail.com>
2025-04-17 08:28:53 +0530

    Update RebaseExample.java

--- RebaseExample.java
+++ RebaseExample.java
@@ -1,9 +1,5 @@
 public class RebaseExample {
     public static void main(String[] args) {
-        System.out.println("welcome to A");
-        System.out.println("welcome to b");
-        System.out.println("welcome to Chukki");
-
         System.out.println("welcome to shiva");
     }
 }
